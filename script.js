38179a5aedb03dfd2db4663e7719b4107c5d97ba
sushantsalotra <88648474+sushantsalotra@users.noreply.github.com>
2022-04-03 13:48:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let songBox = document.querySelector(".songbox");
+let masterPlay = document.querySelector(".masterplay");
 
+let playVisibility = false;
+
+songBox.addEventListener("mouseover", () => {
+    if (playVisibility === false){
+        masterPlay.style.visibility = "visible";
+        playVisibility = true;
+    }
+});
+songBox.addEventListener("mouseout", () => {
+    if( playVisibility === true) {
+        masterPlay.style.visibility = "hidden";
+        playVisibility = false;
+    }
+});
