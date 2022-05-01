d733613d0351af6259bbab326a646dbebd45c899
sushantsalotra <88648474+sushantsalotra@users.noreply.github.com>
2022-05-01 11:05:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-let songBox = document.querySelector(".songbox");
-let masterPlay = document.querySelector(".masterplay");
+let songBox = document.querySelectorAll(".songbox");
+let masterPlay = document.querySelectorAll(".masterplay");
 
 
-songBox.addEventListener("mouseover", () => {
-        masterPlay.style.visibility = "visible";
+
+/*document.querySelectorAll(".songbox").forEach(songboxTile => {
+        songboxTile.addEventListener("mouseover", (event) => {
+                // event.target.querySelector('.masterplay').children[2].children[0].style.visibility = "visible";
+                event.target.querySelector(".masterplay").style.visibility = "visible";
+        });
+        songboxTile.addEventListener("mouseout", (event) => {
+                // event.target.querySelector('.masterplay').children[2].children[0].style.visibility = "visible";
+                event.target.querySelector(".masterplay").style.visibility = "hidden";
+        });
 });
-songBox.addEventListener("mouseout", () => {
-        masterPlay.style.visibility = "hidden";
+// songBox.addEventListener("mouseout", () => {
+//         masterPlay.style.visibility = "hidden";
+// });*/
+
+
+songBox.forEach(songBoxTile => {
+        songBoxTile.addEventListener("mouseover", () =>{
+                masterPlay.forEach(masterPlayBtn => {
+                        masterPlayBtn.style.visibility = "visible";
+                });
+        });
+        songBoxTile.addEventListener("mouseout", () =>{
+                masterPlay.forEach(masterPlayBtn => {
+                        masterPlayBtn.style.visibility = "hidden";
+                });
+        });
 });
