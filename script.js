3ee596c6a387f43f06b5cdf31b1d8e27d5cd0daf
sushantsalotra <88648474+sushantsalotra@users.noreply.github.com>
2022-05-15 13:24:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let songBox = document.querySelectorAll(".songbox");
 let masterPlay = document.querySelectorAll(".masterplay");
 
+let menuBtn = document.querySelector(".menu");
+let closeBtn = document.querySelector("#close");
+let sideNav = document.querySelector(".sidenav");
+let sideUl = document.querySelector('#sidenavlist')
+let logo = document.querySelector("#logo");
+let sideBrand = document.querySelector(".sidebrand");
+
 
 
 /*document.querySelectorAll(".songbox").forEach(songboxTile => {
@@ -30,3 +37,20 @@ songBox.forEach(songBoxTile => {
                 });
         });
 });
+
+menuBtn.addEventListener('click', () => {
+        sideNav.style.width = "90%";
+        sideUl.style.visibility = "visible";
+        sideBrand.style.visibility = "visible";
+        logo.style.visibility = "visible";
+        closeBtn.style.visibility = "visible";
+});
+closeBtn.addEventListener('click', () => {
+        sideNav.style.width = "0%";
+        sideUl.style.visibility = "hidden";
+        sideBrand.style.visibility = "hidden";
+        logo.style.visibility = "hidden";
+        closeBtn.style.visibility = "hidden";
+});
+
+
